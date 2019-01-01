79aa52bc12b052923b090969ea673df3de570489
Scott Parsons <scott@sussexdev.co.uk>
2019-01-01 13:26:34 +0000

    1.0.3

--- Observer/PurgeCloudflareCache.php
+++ Observer/PurgeCloudflareCache.php
@@ -3,7 +3,7 @@
  * @package    SussexDev_Cloudflare
  * @copyright  Copyright (c) 2019 Scott Parsons
  * @license    https://github.com/ScottParsons/module-cloudflare/blob/master/LICENSE.md
- * @version    1.0.0
+ * @version    1.0.3
  */
 namespace SussexDev\Cloudflare\Observer;
 
